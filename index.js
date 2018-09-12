bb3118d73eff8b49fdaf75dadbe467be3577f1c6
Yuichi Yogo <yuichkun@gmail.com>
2018-09-12 21:46:48 +0900

    preserve the original and use it to log strings

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+Function.prototype.clone = function(){
+    const that = this;
+    const tmp = function() {
+        return that.apply(this, arguments);
+    };
+    for (const key in this) {
+        if (this.hasOwnProperty(key)) {
+            tmp[key] = this[key];
+        }
+    }
+    return tmp;
+};
+
+console._log = console.log.clone();
+
 console.log = (...args) => {
     args.forEach(arg => {
+        if (typeof arg === 'string') {
+            debugger;
+            console._log(arg);
+            return;
+        }
         console.dir(arg, {
-            // the default value of console.log.depth is undefined, which makes console dir to trace recursively till the deepest layer 
+            // the default value of console.log.depth is undefined, which makes console.dir to trace recursively till the deepest layer 
             depth: console.log.depth,
             colors: true
         });
     });
 };
-
